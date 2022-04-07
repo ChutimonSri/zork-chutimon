d122a4c802f8a469a3a840d285e299954c478b49
ChutimonSri <chutimon.sro@student.mahidol.edu>
2022-04-07 19:15:27 +0700

    Added Item.java file

---
+++ src/main/java/io/muzoo/chutimonsri/ssc/zork/room/Item.java
@@ -0,0 +1,15 @@
+package io.muzoo.chutimonsri.ssc.zork.room;
+
+public class Item {
+
+    private String ItemName;
+    private int attack;
+
+    public String getItemName() {
+        return ItemName;
+    }
+
+    public int getAttack() {
+        return attack;
+    }
+}
